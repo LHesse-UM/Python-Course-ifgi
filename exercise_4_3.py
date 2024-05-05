02d7f73354f93e0f7cba1048edf15bf10b47b982
LHesse-UM <153600729+LHesse-UM@users.noreply.github.com>
2024-05-05 12:56:10 +0200

    Update exercise_4_3.py

--- exercise_4_3.py
+++ exercise_4_3.py
@@ -4,28 +4,39 @@ from qgis.core import *
 
 import os
 
-# Layer Path setzen
+# set your path to the folder of the layer(s) here!
 layer_path = 'C:/Users/lucah/Downloads/Muenster/Muenster'
 
-# Project Path setzen
+# set your path to the QGIS project here!
 project_path = "C:/Users/lucah/OneDrive/Desktop/Test/myFirstProject.qgz"
 
 
+# create QGIS instance and load the project
 project = QgsProject.instance()
 project.read(project_path)
 
+# list of all files in layer folder
 directory = os.listdir(layer_path)
 
+
+# iterate though all files
 for file in directory:
+
+    # filter for shapefiles
     if file.endswith('.shp'):
+
+        # create vector layer
         layer = QgsVectorLayer(layer_path + '/' + file, file[:-4], "ogr")
 
         if not layer.isValid():
             print("Error loading the layer!")
-        else:
             
+        else:
+
+            # add layer to map
             project.addMapLayer(layer)
 
+            # save project
             project.write()
 
             print("Layer added to project\nProject saved successfully!")
