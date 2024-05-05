3df41fb11aa95d90680362ab6b5111ceb7e78b22
LHesse-UM <153600729+LHesse-UM@users.noreply.github.com>
2024-05-05 12:47:06 +0200

    Update exercise_4_1.py

--- exercise_4_1.py
+++ exercise_4_1.py
@@ -1,6 +1,10 @@
+# import necessary modules
 from qgis.PyQt.QtCore import QUrl
 from qgis.PyQt.QtWebKitWidgets import QWebView
 
+# Create an instance of QWebView
 myWV = QWebView(None)
+
+# Loading url and displaying the pop up window
 myWV.load(QUrl('https://wikipedia.org/wiki/[%name%]'))
 myWV.show()
