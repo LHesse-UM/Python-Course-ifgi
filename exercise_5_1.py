ac1f075e86f41a0ed70c252fcaad7e02ea214ff1
LHesse-UM <153600729+LHesse-UM@users.noreply.github.com>
2024-05-07 11:20:36 +0200

    v 1.0.1: cleaner code
    structured the code and adjusted the comments for a better read

--- exercise_5_1.py
+++ exercise_5_1.py
@@ -1,9 +1,13 @@
 # Create a map canvas object
 mc = iface.mapCanvas()
 
+# create parent window for the sub-windows later on
+parent = iface.mainWindow()
+
+
 # get Muenster_City_Districts.shp layer in the TOC
-layers = QgsProject.instance().mapLayersByName("Muenster_City_Districts")
-layer = layers[0]
+districtLayers = QgsProject.instance().mapLayersByName("Muenster_City_Districts")
+districtLayer = districtLayers[0]
 
 # Create QgsFeatureRequest() instance
 request = QgsFeatureRequest()
@@ -13,68 +17,76 @@ nameClause = QgsFeatureRequest.OrderByClause("Name", ascending = True)
 
 # Set clause
 orderby = QgsFeatureRequest.OrderBy([nameClause])
+
+# assign orderby to the request
 request.setOrderBy(orderby)
 
+# create list for the distric names
 district_names = []
 
-# Print features ordered by attribute "Name"
-for feature in layer.getFeatures(request):
+
+# save features ordered by attribute "Name"
+for feature in districtLayer.getFeatures(request):
     district_names.append(feature["Name"])
 
-#print(district_names)
-
-
-
+# save dialog input as well as boolean value whether 'ok' was clicked or not 
 sDistrict, bOk = QInputDialog.getItem(parent, "District Names", "Select District", district_names)
 
+
+# do nothing if the user cancled the input dialog
 if bOk == False:
     QMessageBox.warning(parent, "Schools", "User cancelled")
 
 else:
 
+    # get Schools.shp layer in the TOC
+    schoolLayers = QgsProject.instance().mapLayersByName("Schools")
+    schoolLayer = schoolLayers[0]
+    
+    # get the different features from the school layer
+    schoolFeatures = schoolLayer.getFeatures()
+    
 
-    schoolLayer = QgsProject.instance().mapLayersByName("Schools")
-    schools = schoolLayer[0]
+    # select the district, which was chosen in the dialog
+    districtLayer.selectByExpression(f"\"Name\" = '{sDistrict}'", QgsVectorLayer.SetSelection)
     
-    schoolFeatures = schools.getFeatures()
-    
-    
-    
-    #print(f"\"Name\" == '{sDistrict}'")
+    # retreive the feature of the district
+    feature = districtLayer.selectedFeatures()[0]
 
-
-    layer.selectByExpression(f"\"Name\" = '{sDistrict}'", QgsVectorLayer.SetSelection)
-    feature = layer.selectedFeatures()[0]
+    # save geometry of the district feature
     districtGeometry = feature.geometry()
     
-    message = ""
     
+    # create a Distance Area and assign the ETRS89 reference system to it for later distance calculations
     da = QgsDistanceArea()
     da.setEllipsoid("ETRS89")
-    
+
+
+    # create centroid of the district geometry and save it's x and y value
+    centroid = districtGeometry.centroid()
+    centroidX = centroid.get().x()
+    centroidY = centroid.get().y()
+
+    # create variable for result message
+    message = ""
+
+    # iterate through all schools
     for school in schoolFeatures:
+
+        # save school geometry and it's x and y value
         schoolGeometry = school.geometry()
+        schoolGeometryX = schoolGeometry.get().x()
+        schoolGeometryY = schoolGeometry.get().y()
         
+        # check whether school geometry is inside the district's
         if districtGeometry.contains(schoolGeometry):
             
-            
-            centroid = districtGeometry.centroid()
-            centroidX = centroid.get().x()
-            centroidY = centroid.get().y()
-            
-            schoolGeometryX = schoolGeometry.get().x()
-            schoolGeometryY = schoolGeometry.get().y()
-            
-
-            print(centroid)
+            # calculate distance between centroid and the school
             distance = da.measureLine([QgsPointXY(centroidX,centroidY),QgsPointXY(schoolGeometryX,schoolGeometryY)])/1000
-            print(distance)
+
+            # attach school metadata as well as the distance to the result message
             message = message + school.attributes()[1] + ", " + school.attributes()[2] + "\nDistance to district centrum " + str(round(distance, 2)) + " km\n\n"
     
     
+    # present the result message in a information window
     QMessageBox.information(parent, f"Schools in {sDistrict}", message)
-
-
-print(sDistrict)
-
-print(bOk)
