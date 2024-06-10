0a7b0851d83a8a4ba51786ae3e1c3ff70573330a
LHesse-UM <153600729+LHesse-UM@users.noreply.github.com>
2024-06-10 23:13:18 +0200

    1.0.2 added comments

--- exercise_9_1.py
+++ exercise_9_1.py
@@ -1,22 +1,38 @@
 # exercise 9.1
+
+# import modules
 import arcpy
 import os
 
-arcpy.env.workspace = r'C:\Users\lucah\OneDrive\Desktop\exercise_arcpy_1.gdb' #Pfad anpassen
 
+# set workspace (SET YOUR OWN WORKING DIRECTORY)
+arcpy.env.workspace = r'C:\Users\lucah\OneDrive\Desktop\exercise_arcpy_1.gdb' 
+
+# Access all feature classes
 fc_list = arcpy.ListFeatureClasses(feature_type='Point')
+
+# Remove active_assets from the list, because we dont want to iterate through this feature class
 fc_list.remove('active_assets')
 
+# create path to active_assets
 fc_path_assets = os.path.join(arcpy.env.workspace, 'active_assets')
 
+# sql filter statement
 sql = "status='active'"
 
+# create InsertCursor for active_assets
 icur = arcpy.da.InsertCursor(in_table=fc_path_assets,field_names='*')
+
+# iterate through feature classes
 for fc in fc_list:
-    
+
+    # set path for current feature class
     fc_path = os.path.join(arcpy.env.workspace,fc)
-    
+
+    # searchCursor for current feature class with defined sql filter
     scur = arcpy.da.SearchCursor(in_table=fc_path,field_names='*',where_clause=sql)
     for row in scur:
+        # insert row into active_assets
         icur.insertRow(row)
+# delete InsertCursor
 del icur
