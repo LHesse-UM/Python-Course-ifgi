56b971b03f4e4a5bb833bab54368f3d46d4c6e16
LHesse-UM <153600729+LHesse-UM@users.noreply.github.com>
2024-06-10 23:17:56 +0200

    1.0.1 added comments

--- exercise_9_2.py
+++ exercise_9_2.py
@@ -1,26 +1,39 @@
+# import modules
 import arcpy
 import os
 
+# set workspace (ADD YOUR OWN WORKSPACE HERE!)
 arcpy.env.workspace = r'C:\Users\lucah\OneDrive\Desktop\exercise_arcpy_1.gdb' #Pfad anpassen
 
+# Path to active_assets
 fc_path_assets = os.path.join(arcpy.env.workspace, 'active_assets')
 
+# Add a helper filed 'Buffer_Distance' for later calculations
 arcpy.management.AddField(fc_path_assets, 'Buffer_Distance', 'INTEGER')
 
+# Buffer Dictionary 
 buffer_distances = {
     'mast': 300,
     'mobile_antenna': 50,
     'building_antenna': 100
 }
 
-
+# Create UpdateCursor for active_assets
 ucur = arcpy.da.UpdateCursor(in_table=fc_path_assets,field_names='*')
+
+# Iterate though active_assets
 for row in ucur:
-    
+
+    # Create entry for 'Buffer_Distance' by comparing the current type value with the Dictionary
     row[4] = buffer_distances[row[3]]
+
+    # Update the row
     ucur.updateRow(row)
+
+# Delete the UpdateCursor
 del ucur
 
+# Apply the Buffer-Tool: Create output-class 'coverage' and take the new field value 'Buffer_Distance' to choose the Distance for each feature
 arcpy.analysis.Buffer(
     in_features="active_assets",
     out_feature_class=r"C:\Users\lucah\OneDrive\Desktop\exercise9\Default.gdb\coverage",
